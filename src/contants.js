ddfe136af352f656de1ae9c2cc4a21353c0fdbf1
Sameet <sameetghouri@yahoo.com>
2023-10-20 22:39:01 +0500

    m

--- src/contants.js
+++ src/contants.js
@@ -1,3 +1,3 @@
 export const api_url = "https://joyous-yak-toga.cyclic.app"
 // export const api_url = "https://puce-beautiful-iguana.cyclic.app"
-// export const api_url = `http://localhost:5000`;
+// export const api_url = `http://localhost:4001`;
