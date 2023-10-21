037d04af964d0ec8bf92f0d16b8bf11b654e5117
faizanmalikk <faizanjamil656@gmail.com>
2023-10-21 18:59:47 +0500

    fix cors error

--- src/contants.js
+++ src/contants.js
@@ -1,3 +1,3 @@
-export const api_url = "https://joyous-yak-toga.cyclic.app"
-// export const api_url = "https://puce-beautiful-iguana.cyclic.app"
+// export const api_url = "https://joyous-yak-toga.cyclic.app"
+export const api_url = "https://intell-backend.vercel.app"
 // export const api_url = `http://localhost:4001`;
